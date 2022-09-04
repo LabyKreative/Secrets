e003a02a5127c18671cdf00a721ef93ac378cec9
Oyelamin <blessingcodephp@gmail.com>
2022-09-04 19:01:08 +0100

    Continue adjustments

--- app.js
+++ app.js
@@ -178,11 +178,11 @@ app.post("/login", function(req, res) {
 
 
 let port = process.env.PORT;
-  if (port == null || port == "") {
-    port = 3000;
-  }
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000.");
+app.listen(port, function() {
+  console.log("Server has started successfully on port 3000!");
 });
