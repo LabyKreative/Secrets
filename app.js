003e65f088e2746c8609b4fe07fe53ad7d866db6
LabyKreative <labykreative@yahoo.com>
2022-09-07 12:48:57 +0100

    Adjust PackageJs

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb+srv://LabyKreative:Test123@cluster0.ebc8min.mongodb.net/userDB", {useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true
 }, err => {
   if (err) throw err;
     console.log("connected to MongoDB!!!")
